f57ca94e594fa4dce3e4f657f24182d65b4e0238
Cherkesky <guy@cherkesky.com>
2019-10-17 11:02:48 -0500

    activator and deactivator selectors and functions been added

--- src/scripts/powers.js
+++ src/scripts/powers.js
@@ -1,5 +1,5 @@
 console.log("super powers");
-
+// individial buttons functions
 const flightHandlerFunction = () => {
   document.querySelector("#flight").classList.replace("disabled", "enabled")
 }
@@ -9,8 +9,30 @@ const mindReaderFunction = () => {
 const xRayFunction = () => {
   document.querySelector("#xray").classList.replace("disabled", "enabled")
 }
+// toggle on/off buttons functions
+const activatorFunction = () => {
+  const currentState = document.querySelectorAll(".power")
+  currentState.forEach(element => {
+    if (element.classList === "power", "disable") {
+      element.classList.replace("disabled", "enabled")
+    }
+  });
+}
 
-document.querySelector("#activate-flight").addEventListener("click",flightHandlerFunction);
-document.querySelector("#activate-mindreading").addEventListener("click",mindReaderFunction);
-document.querySelector("#activate-xray").addEventListener("click",xRayFunction);
+const deactivatorFunction = () => {
+  const currentState = document.querySelectorAll(".power")
+  currentState.forEach(element => {
+    if (element.classList === "power", "enabled") {
+      element.classList.replace("enabled", "disabled")
+    }
+  });
+}
 
+// individual event listeners for the first 3 'activator' buttons
+document.querySelector("#activate-flight").addEventListener("click", flightHandlerFunction);
+document.querySelector("#activate-mindreading").addEventListener("click", mindReaderFunction);
+document.querySelector("#activate-xray").addEventListener("click", xRayFunction);
+
+// toggle on/off event listeneres 
+document.querySelector("#activate-all").addEventListener("click", activatorFunction);
+document.querySelector("#deactivate-all").addEventListener("click", deactivatorFunction);
